234307aea4ac96907be5de866c46d461521e88f6
Luca-Saboia <luca.silva.saboia@gmail.com>
2026-05-06 10:48:17 +0000

    Versao 0.1

---
+++ src/main/java/br/mackenzie/projetops2/Projetops2Application.java
@@ -0,0 +1,13 @@
+package br.mackenzie.projetops2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetops2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetops2Application.class, args);
+	}
+
+}
